24cee9dabc2bbf66943940154a27a6863546d93a
CrysTalesiaS <53529472+CrysTalesiaS@users.noreply.github.com>
2021-03-12 15:27:31 +0800

    Update usage.py

--- bot/modules/usage.py
+++ bot/modules/usage.py
@@ -64,9 +64,9 @@ def dyno_usage(update, context):
             
             sendMessage(
                 f"<b>Penggunaan Dyno buat aplikasi </b> <code>{app.name}</code> :\n"
-                f"• <code>{AppHours}</code> <b>Jam </b> <code>{AppMinutes}</code> <b>Menit </b> <code>{AppSeconds}<code> <b>Detik- {AppPercent}%</b>\n\n"
+                f"• <code>{AppHours}</code> <b>Jam </b> <code>{AppMinutes}</code> <b>Menit - {AppPercent}%</b>\n\n"
                 "<b>Sisa Kouta/Dyno bot:</b>\n"
-                f"• <code>{hours}</code> <b>Jam </b> <code>{minutes}</code> <b>Menit </b> <code>{AppSecond}<code> <b>Detik- {quota_percent}%</b>",
+                f"• <code>{hours}</code> <b>Jam </b> <code>{minutes}</code> <b>Menit - {quota_percent}%</b>",
                 context.bot,
                 update
             )

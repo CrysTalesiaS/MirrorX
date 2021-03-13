e14760404406426c559c4ab997efac30c0f5fbca
CrysTalesiaS <53529472+CrysTalesiaS@users.noreply.github.com>
2021-03-14 01:59:13 +0800

    Update search.py

--- bot/modules/search.py
+++ bot/modules/search.py
@@ -145,7 +145,7 @@ def searchhelp(update, context):
 • /sts <i>[search query]</i>
 • /sukebei <i>[search query]</i>
 '''
-    update.effective_message.reply_photo("https://t.me/BotMirror/9064", help_string, parse_mode=ParseMode.HTML)
+    update.effective_message.reply_photo("https://telegra.ph/file/4ef03fcaed334989346ec.jpg", help_string, parse_mode=ParseMode.HTML)
     
     
 SEARCHHELP_HANDLER = CommandHandler("tolongtorrent", searchhelp)

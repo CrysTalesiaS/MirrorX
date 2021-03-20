73055f3998e39a96fd19a13d0fbd335aae2e2e73
CrysTalesiaS <53529472+CrysTalesiaS@users.noreply.github.com>
2021-03-20 15:12:20 +0800

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -98,7 +98,7 @@ def log(update, context):
 @run_async
 def system_status(update, context):
     uname = platform.uname()
-    system = f'<b>======[ SYSTEM INFO ]======</b>\n\n' \
+    status = f'<b>======[ SYSTEM INFO ]======</b>\n\n' \
              f'<b>System uptime:</b> <code>' + str(uptime) + '</code>\n' \
              f'<b>System:</b> <code>' + str(uname.system) + '</code>\n' \
              f'<b>Node name:</b> <code>' + str(uname.node) + '</code>\n' \

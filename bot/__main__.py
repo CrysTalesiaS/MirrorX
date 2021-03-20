f9b187019d4e87acab363f5194537e5714c9187f
CrysTalesiaS <53529472+CrysTalesiaS@users.noreply.github.com>
2021-03-20 16:09:14 +0800

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -99,7 +99,6 @@ def log(update, context):
 def systemstats(update, context):
     uname = platform.uname()
     status = f'<b>======[ SYSTEM INFO ]======</b>\n\n' \
-             f'<b>System uptime:</b> <code>' + str(uptime) + '</code>\n' \
              f'<b>System:</b> <code>' + str(uname.system) + '</code>\n' \
              f'<b>Node name:</b> <code>' + str(uname.node) + '</code>\n' \
              f'<b>Release:</b> <code>' + str(uname.release) + '</code>\n' \

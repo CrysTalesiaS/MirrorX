3cd9958503f3932d6560fbe1b73def67de1823a2
CrysTalesiaS <53529472+CrysTalesiaS@users.noreply.github.com>
2021-03-21 15:02:36 +0800

    Update songs.py

--- bot/modules/songs.py
+++ bot/modules/songs.py
@@ -53,7 +53,7 @@ async def download_lyrics(v_url) :
     if v_url.is_group:
         if (await is_register_admin(v_url.input_chat, v_url.message.sender_id)):
             pass
-          elif v_url.chat_id == iid and v_url.sender_id == userss:
+        elif v_url.chat_id == iid and v_url.sender_id == userss:
             pass
         else:
             return

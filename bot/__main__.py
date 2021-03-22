12892030a89d4ae89f2210fef04153c8e36319e7
CrysTalesiaS <53529472+CrysTalesiaS@users.noreply.github.com>
2021-03-23 00:30:30 +0800

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -23,7 +23,7 @@ from bot.helper.telegram_helper.bot_commands import BotCommands
 from bot.helper.telegram_helper.message_utils import *
 from .helper.ext_utils.bot_utils import get_readable_file_size, get_readable_time
 from .helper.telegram_helper.filters import CustomFilters
-from .modules import authorize, list, wiki, gtranslator, paste, cancel_mirror, mirror_status, mirror, clone, watch, shell, eval, anime, stickers, search, delete, speedtest, usage
+from .modules import authorize, extra, list, wiki, gtranslator, paste, cancel_mirror, mirror_status, mirror, clone, watch, shell, eval, anime, stickers, search, delete, speedtest, usage
 
 now=datetime.now(pytz.timezone('Asia/Jakarta'))
 

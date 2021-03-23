82cb718c392fa44b8e59ee7825a7632751345e78
CrysTalesiaS <53529472+CrysTalesiaS@users.noreply.github.com>
2021-03-23 17:18:58 +0800

    Update last_fm_sql.py

--- bot/helper/last_fm_sql.py
+++ bot/helper/last_fm_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import Column, String
 
-from lynda.modules.sql import BASE, SESSION
+from bot.helper.last_fm_sql import BASE, SESSION
 
 
 class LastFMUsers(BASE):

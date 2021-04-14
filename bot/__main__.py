b3120a1db743b8f0df328314ec239b32d6aa2d5f
CrysTalesiaS <53529472+CrysTalesiaS@users.noreply.github.com>
2021-04-14 22:11:06 +0800

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -27,7 +27,12 @@ from .modules import authorize, extra, list, wiki, paste, cancel_mirror, mirror_
 from bot.modules.alternate import typing_action
 
 now=datetime.now(pytz.timezone('Asia/Jakarta'))
+_IS_TELEGRAPH = True
+_IS_STICKER = True
 
+_DEFAULT = "https://t.me/c/1475139935/22255"
+_CHAT, _MSG_ID = None, None
+_LOGO_ID = https://t.me/c/1475139935/22255
 
 @run_async
 def stats(update, context):
@@ -224,29 +229,119 @@ def mirror_help(update, context):
 
 '''
     sendMessage(mirror_string, context.bot, update)
-
+    
 @run_async
-async def systemkutest(update, context):
-    fetch = await asyncrunapp(
-                "neofetch",
-                "--stdout",
-                stdout=asyncPIPE,
-                stderr=asyncPIPE,
-            )
-
-            stdout, stderr = fetch.communicate()
-            result = str(stdout.decode().strip()) + str(stderr.decode().strip())
-
-            await sysd.edit("`" + result + "`")
-         except FileNotFoundError:
-            await sysd.edit("`Install neofetch first !!`")
-       context.bot.sendMessage(
-        update.effective_chat.id, code, parse_mode=ParseMode.HTML
-       )
-systemkutest_handler = CommandHandler(BotCommands.systemkutestCommand, systemkutest,
-                                      filters=CustomFilters.authorized_chat | CustomFilters.authorized_user)
+def _get_alive_text_and_markup(message: Message) -> Tuple[str, Optional[InlineKeyboardMarkup]]:
+    markup = None
+    output = f"""
+**⏱ Uptime** : `{userge.uptime}`
+**💡 Version** : `{get_version()}`
+**__Python__**: `{versions.__python_version__}`
+    **__Pyrogram__**: `{versions.__pyro_version__}`"""
     
-    
+
+async def _send_alive(message: Message,
+                      text: str,
+                      reply_markup: Optional[InlineKeyboardMarkup],
+                      recurs_count: int = 0) -> None:
+    if not _LOGO_ID:
+        await _refresh_id(message)
+    should_mark = None if _IS_STICKER else reply_markup
+    if _IS_TELEGRAPH:
+        await _send_telegraph(message, text, reply_markup)
+    else:
+        try:
+            await message.client.send_cached_media(chat_id=message.chat.id,
+                                                   file_id=_LOGO_ID,
+                                                   caption=text,
+                                                   reply_markup=should_mark)
+            if _IS_STICKER:
+                raise ChatSendMediaForbidden
+        except SlowmodeWait as s_m:
+            await asyncio.sleep(s_m.x)
+            text = f'<b>{str(s_m).replace(" is ", " was ")}</b>\n\n{text}'
+            return await _send_alive(message, text, reply_markup)
+        except MediaEmpty:
+            if recurs_count >= 2:
+                raise ChatSendMediaForbidden
+            await _refresh_id(message)
+            return await _send_alive(message, text, reply_markup, recurs_count + 1)
+        except (ChatSendMediaForbidden, Forbidden):
+            await message.client.send_message(chat_id=message.chat.id,
+                                              text=text,
+                                              disable_web_page_preview=True,
+                                              reply_markup=should_mark)
+            
+                       
+
+async def _refresh_id(message: Message) -> None:
+    global _LOGO_ID, _IS_STICKER  # pylint: disable=global-statement
+    try:
+        media = await message.client.get_messages(_CHAT, _MSG_ID)
+    except (ChannelInvalid, PeerIdInvalid, ValueError):
+        _set_data(True)
+        return await _refresh_id(message)
+    else:
+        if media.sticker:
+            _IS_STICKER = True
+        _LOGO_ID = get_file_id_of_media(media)
+        
+def _set_data(errored: bool = False) -> None:
+    global _CHAT, _MSG_ID, _IS_TELEGRAPH  # pylint: disable=global-statement
+
+    pattern_1 = r"^(http(?:s?):\/\/)?(www\.)?(t.me)(\/c\/(\d+)|:?\/(\w+))?\/(\d+)$"
+    pattern_2 = r"^https://telegra\.ph/file/\w+\.\w+$"
+    if Config.ALIVE_MEDIA and not errored:
+        if Config.ALIVE_MEDIA.lower().strip() == "nothing":
+            _CHAT = "text_format"
+            _MSG_ID = "text_format"
+            return
+        media_link = Config.ALIVE_MEDIA
+        match_1 = re.search(pattern_1, media_link)
+        match_2 = re.search(pattern_2, media_link)
+        if match_1:
+            _MSG_ID = int(match_1.group(7))
+            if match_1.group(5):
+                _CHAT = int("-100" + match_1.group(5))
+            elif match_1.group(6):
+                _CHAT = match_1.group(6)
+        elif match_2:
+            _IS_TELEGRAPH = True
+        elif "|" in Config.ALIVE_MEDIA:
+            _CHAT, _MSG_ID = Config.ALIVE_MEDIA.split("|", maxsplit=1)
+            _CHAT = _CHAT.strip()
+            _MSG_ID = int(_MSG_ID.strip())
+    else:
+        match = re.search(pattern_1, _DEFAULT)
+        _CHAT = match.group(6)
+        _MSG_ID = int(match.group(7))
+
+
+async def _send_telegraph(msg: Message, text: str, reply_markup: Optional[InlineKeyboardMarkup]):
+    path = os.path.join(Config.DOWN_PATH, os.path.split(Config.ALIVE_MEDIA)[1])
+    if not os.path.exists(path):
+        await pool.run_in_thread(wget.download)(Config.ALIVE_MEDIA, path)
+    if path.lower().endswith((".jpg", ".jpeg", ".png", ".bmp")):
+        await msg.client.send_photo(
+            chat_id=msg.chat.id,
+            photo=path,
+            caption=text,
+            reply_markup=reply_markup
+        )
+    elif path.lower().endswith((".mkv", ".mp4", ".webm")):
+        await msg.client.send_video(
+            chat_id=msg.chat.id,
+            video=path,
+            caption=text,
+            reply_markup=reply_markup
+        )
+    else:
+        await msg.client.send_document(
+            chat_id=msg.chat.id,
+            document=path,
+            caption=text,
+            reply_markup=reply_markup
+        )
     
 def main():
     fs_utils.start_cleanup()
